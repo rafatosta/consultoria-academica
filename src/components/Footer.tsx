a25a2819bbe6eb88b84fa539885b1b5925ac94bb
Rafael Tosta <rafa.ecomp@gmail.com>
2023-01-07 16:19:10 -0300

    :up: Footer dev

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,26 +4,29 @@ import { useLocation, Link } from "react-router-dom";
 export function Footer() {
   const location = useLocation();
   return (
-    <div className="flex flex-col w-full justify-center items-center bg-blue-900 text-white pt-4">
-      <div id="menu" className="flex flex-col items-center gap-y-4">
-        <p className="font-extrabold text-lg">Menu</p>
-        <div className="grid grid-cols-2 gap-2">
-          {routesNavigation.map((item) => {
-            return (
-              <Link
-                key={item.name}
-                to={item.path}
-                className={
-                  location.pathname == item.path
-                    ? "text-white font-extrabold"
-                    : "text-gray-300"
-                }
-              >
-                {item.name}
-              </Link>
-            );
-          })}
+    <div className="flex flex-col w-full h-full justify-center items-center bg-blue-900 text-white pt-4 gap-y-2">
+      <div className="flex flex-col items-center gap-y-4 sm:flex-row sm:justify-around sm:items-center sm:w-full">
+        <div id="menu" className="flex flex-col items-center gap-y-2 my-8">
+          <p className="font-extrabold text-lg">Menu</p>
+          <div className="grid grid-cols-2 gap-2">
+            {routesNavigation.map((item) => {
+              return (
+                <Link
+                  key={item.name}
+                  to={item.path}
+                  className={
+                    location.pathname == item.path
+                      ? "text-white font-extrabold"
+                      : "text-gray-300"
+                  }
+                >
+                  {item.name}
+                </Link>
+              );
+            })}
+          </div>
         </div>
+
         <div id="contact" className="flex flex-col items-center gap-y-2">
           <p className="font-extrabold text-lg">Contatos</p>
           <p>Telefone: (xx) x xxxx-xxxx </p>
@@ -68,8 +71,73 @@ export function Footer() {
           </div>
         </div>
       </div>
-      <span className="inline-block bg-gray-100/30 w-full h-[0.05rem] my-4"></span>
-      <div className="h-10">by rtosta</div>
+      <span className="inline-block bg-gray-100/30 w-full h-[0.05rem]"></span>
+      <div className="flex flex-col justify-center items-center mb-2 sm:flex-row sm:w-full sm:justify-around sm:items-center sm:px-4">
+        <a
+          className="hover:font-bold"
+          href="https://rafatosta.github.io/"
+          target="_blank"
+        >
+          Desenvolvido por Rafael Tosta{" "}
+        </a>
+
+        <div className="flex flex-row">
+          {/* linkedin */}
+          <a
+            className="mx-1.5 transform text-white transition-colors duration-300 hover:text-blue-500"
+            href="https://www.linkedin.com/in/rafael-tosta-2006874a/"
+            target="_blank"
+          >
+            <svg
+              className="h-6 w-6"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 16h-2v-6h2v6zm-1-6.891c-.607 0-1.1-.496-1.1-1.109 0-.612.492-1.109 1.1-1.109s1.1.497 1.1 1.109c0 .613-.493 1.109-1.1 1.109zm8 6.891h-1.998v-2.861c0-1.881-2.002-1.722-2.002 0v2.861h-2v-6h2v1.093c.872-1.616 4-1.736 4 1.548v3.359z"
+                fill="currentColor"
+              />
+            </svg>
+          </a>
+          {/* instagram */}
+          <a
+            className="mx-1.5 transform text-white transition-colors duration-300 hover:text-blue-500"
+            href="https://www.instagram.com/rafatosta_/"
+            target="_blank"
+          >
+            <svg
+              className="h-6 w-6"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"
+                fill="currentColor"
+              />
+            </svg>
+          </a>
+          {/* github */}
+          <a
+            className="mx-1.5 transform text-white transition-colors duration-300 hover:text-blue-500"
+            href="https://github.com/rafatosta"
+            target="_blank"
+          >
+            <svg
+              className="h-6 w-6"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+                fill="currentColor"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
