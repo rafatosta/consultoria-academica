b93b45350a1ca665549365c72171f733da42e938
Rafael Tosta <rafa.ecomp@gmail.com>
2023-01-15 12:22:55 -0300

    add: prazos de entregas

--- pages/index.tsx
+++ pages/index.tsx
@@ -104,6 +104,46 @@ export default function Home() {
             })}
           </div>
         </div>
+        <div id="deadlines" className=" flex justify-center items-center p-8 py-16">
+          <div>
+            <p className="font-sans text-xl sm:text-2xl font-extrabold">
+              {" "}
+              Solicitando meus serviços você terá:
+            </p>
+            <div className="flex flex-col justify-center gap-y-8 sm:flex-row sm:justify-start sm:gap-x-8 pt-8">
+              <div>
+                <p className="inline-flex uppercase tracking-wide text-lg text-blue-700 font-bold items-center">
+                  Revisão e formatação{" "}
+                </p>
+                <ul className="list-disc list-inside ml-4">
+                  <li>Prazo de entrega: 5 dias;</li>
+                  <li>Taxa de urgência: 10% sobre o valor final.</li>
+                </ul>
+              </div>
+              <div>
+                <p className="inline-flex uppercase tracking-wide text-lg text-blue-700 font-bold items-center">
+                  Construção de trabalhos
+                </p>
+                <ul className="list-disc list-inside ml-4">
+                  <li>Até 3 páginas: 3 dias.</li>
+                  <li>Até 5 páginas: 4 dias.</li>
+                  <li>Mais de 5 páginas: 5 dias.</li>
+                  <li>Taxa de urgência: 20% do valor total.</li>
+                </ul>
+              </div>
+              <div>
+                <p className="inline-flex uppercase tracking-wide text-lg text-blue-700 font-bold items-center">
+                  Formas de pagamento
+                </p>
+                <ul className="list-disc list-inside ml-4">
+                  <li>Pix</li>
+                  <li>Cartão de crédito (via PicPay)</li>
+                  <li>Transferência ou depósito bancário</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
